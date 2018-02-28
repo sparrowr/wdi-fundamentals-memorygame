1c0524b4cb64c7a5a3eecda76234f70dfcf30229
sparrow <sparrow@srubin.org>
2018-02-28 15:14:19 -0500

    created functions!

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
 console.log("Up and running!");
 
-
-
 var cards = ["queen","queen","king","king"]; 
 var cardsInPlay = []; 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("you found a match!");
-	}else{
-		alert("sorry, try again."); 
-	}
-}
+
+
+var checkForMatch(){
+ 	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			console.log("you found a match!");
+		}else{
+			console.log("sorry, try again."); 
+		}
+	}	
+}; 
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]); 
+	cardsInPlay.push(cards[cardId]); 
+	checkForMatch(); 
+};
+
+flipCard(0); 
+flipCard(2); 
