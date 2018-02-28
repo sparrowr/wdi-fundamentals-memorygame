c5b62b620e67837e57092622486a2df14d6da2e4
sparrow <sparrow@srubin.org>
2018-02-28 16:20:42 -0500

    now using objects!

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"]; 
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage:"images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage:"images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts", 
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}]; 
+
 var cardsInPlay = []; 
 
 
-var checkForMatch(){
+var checkForMatch = function(){
  	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]){
 			console.log("you found a match!");
@@ -15,8 +36,10 @@ var checkForMatch(){
 }; 
 
 var flipCard = function(cardId){
-	console.log("User flipped " + cards[cardId]); 
-	cardsInPlay.push(cards[cardId]); 
+	console.log("User flipped " + cards[cardId].rank); 
+	cardsInPlay.push(cards[cardId].rank); 
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage); 
 	checkForMatch(); 
 };
 
