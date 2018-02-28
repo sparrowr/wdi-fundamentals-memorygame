722da91468c29acd814c809b91a37cd54fcb76b8
sparrow <sparrow@srubin.org>
2018-02-28 12:47:26 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+
+
+
+var cards = ["queen","queen","king","king"]; 
+var cardsInPlay = []; 
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match!");
+	}else{
+		alert("sorry, try again."); 
+	}
+}
