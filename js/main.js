3881fa49d6b32f420521a52081f4d47b41b77401
sparrow <sparrow@srubin.org>
2018-03-01 12:40:54 -0500

    now with events!

--- js/main.js
+++ js/main.js
@@ -35,13 +35,25 @@ var checkForMatch = function(){
 	}	
 }; 
 
-var flipCard = function(cardId){
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id'); 
+
 	console.log("User flipped " + cards[cardId].rank); 
 	cardsInPlay.push(cards[cardId].rank); 
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage); 
+	this.setAttribute('src',cards[cardId].cardImage); 
 	checkForMatch(); 
 };
 
-flipCard(0); 
-flipCard(2); 
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img'); 
+		cardElement.setAttribute('src','images/back.png'); 
+		cardElement.setAttribute('data-id',i); 
+		cardElement.addEventListener('click',flipCard); 
+		document.getElementById('game-board').appendChild(cardElement); 
+	}
+}
+
+createBoard(); 
